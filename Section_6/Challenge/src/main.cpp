ae40841fd44f29d3666a4541c3e1f3a9b7788aed
Matt Walsh <mysticrecords@hotmail.com>
2022-12-12 22:10:39 +0000

    addressing compiler warnings
    initialising variables in correctly

--- Section_6/Challenge/src/main.cpp
+++ Section_6/Challenge/src/main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
 
-    const int smRoomCost {25};
-    const int lrgRoomCost {35};
-    const double salesTax {0.06};
-    const int validFor {30};
+    int smRoomCost = 25;
+    int lrgRoomCost = 35;
+    double salesTax = 0.06;
+    int validFor = 30;
 
     string smRoom = "Small";
     string lrgRoom = "Large";
     
-    int noSmRoom {0};
-    int noLrgRoom {0};
+    int noSmRoom = 0;
+    int noLrgRoom = 0;
 
     cout << "Franks Carpet Cleaning Service\nCharges\n$" <<smRoomCost <<"\n$" <<lrgRoomCost 
     <<"\nSales tax rate is " <<salesTax*100 << "%\nEstimates are valid for" << validFor <<" days."<<endl;
@@ -23,7 +23,7 @@ int main() {
     cout << "How many" << lrgRoom << "rooms would you like cleaned?" <<endl;
     cin >> noLrgRoom;
 
-    int salesTotal {(smRoomCost * noSmRoom) + (lrgRoomCost * noLrgRoom)};
+    int salesTotal = (smRoomCost * noSmRoom) + (lrgRoomCost * noLrgRoom);
 
     cout << "Estimate for carpet cleaning service\nNumber of " <<smRoom << " rooms: " <<noSmRoom 
     <<"\nNumber of " <<lrgRoom <<" rooms: " <<noLrgRoom <<"\nPrice per " <<smRoom <<" room: $" <<smRoomCost
