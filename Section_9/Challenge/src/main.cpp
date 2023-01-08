11a5aeffb9674fe4b729be05933751957af9f1c3
Matt Walsh <mysticrecords@hotmail.com>
2023-01-08 19:37:11 +0000

    adding menu function to be printed in main.cpp

--- Section_9/Challenge/src/main.cpp
+++ Section_9/Challenge/src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+std::string menu() {
+
+    std::cout << "P - print numbers\nA - add a number\n M- display mean of the numbers" 
+    << "\nS - display the smallest number\nL - display the largest number\n Q - quit" << std::endl;
+}
+
 int main() {
 
     return 0;
