957ff13c163346f36a49110082a18bac8279c6be
Matt Walsh <mysticrecords@hotmail.com>
2023-01-23 22:45:06 +0000

    completed encodeMsg function
    completed decodeMsg function
    
    give user option to decode message or have it lost forever never knowing what it said....

--- Section_10/Challenge/src/main.cpp
+++ Section_10/Challenge/src/main.cpp
@@ -1,30 +1,77 @@
 #include <iostream>
 #include <string.h>
 
-const std::string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!.? "};
-const std::string encoder {"?MNB0V8CXZAS7DF1GHJKLQWERPO2IUYTqpwoi3ertyugh4fjdksl5azxm6n9cvb "};
-
 std::string encodetMsg(std::string msg) 
 {
 
-    for(char i: msg) 
-    {
+    std::string alphabet {" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!.?"};
+    std::string encoder {" ?MNB0V8CXZAS7DF1GHJKLQWERPO2IUYTqpwoi3ertyugh4fjdksl5azxm6n9cvb"};
 
+    std::string encodeMsg {};
+
+    for(size_t i = 0; i < msg.length(); i++) 
+    {
+        // std::cout << msg.at(i);
+        for(size_t a = 0; a < alphabet.length(); a++) {
+            if(msg.at(i) == alphabet.at(a)) {
+                // std::cout << "*";
+                encodeMsg += encoder.at(a);
+                // std::cout << encoder.at(a);
+            } else {}
+            
+
+        }
     }
+
+    return encodeMsg;
 
 }
     
 
 std::string decodeMsg(std::string msg) 
 {
+
+    std::string alphabet {" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!.?"};
+    std::string encoder {" ?MNB0V8CXZAS7DF1GHJKLQWERPO2IUYTqpwoi3ertyugh4fjdksl5azxm6n9cvb"};
+    std::string encodeMsg {};
+
+    for(size_t i =0; i < msg.length(); i++) 
+    {
+        // std::cout << msg.at(i);
+        for(size_t a = 0; a < encoder.length(); a++) {
+            if(msg.at(i) == encoder.at(a)) {
+                // std::cout << "*";
+                encodeMsg += alphabet.at(a);
+                // std::cout << encoder.at(a);
+            } else {}
+            
+
+        }
+    }
+
+    return encodeMsg;
     
 }
 
 int main() 
 {
-    std::string secretMsg;
-    std::cout << "Please enter your secret message to encode" << std:: endl;
-    getline(cin, secretMsg);
+    std::string secretMsg{};
+    std::string choose {};
+    std::cout << "Please enter your secret message to encode" << std::endl;
+    getline(std::cin, secretMsg);
+
+    std::cout << encodetMsg(secretMsg) << std::endl;
+
+    std::cout << "would you like to decode your message? Y/N:" << std::endl;
+
+    std::cin >> choose;
+
+    if(choose.compare("y") || choose.compare("Y")) {
+        std::cout << "your secret nessage reads:" << std::endl;
+        std::cout <<(decodeMsg(encodetMsg(secretMsg))) << std::endl;
+    } else if(choose.compare("n") || choose.compare("N")) {
+        std::cout << "Well, I guess we'll never know what the message read" << std::endl;
+    } 
 
 
     return 0;
