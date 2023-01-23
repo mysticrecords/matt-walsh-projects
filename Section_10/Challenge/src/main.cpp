81e52e3acd26c0cd9a65a144c91aff10fc6b7dc0
Matt Walsh <mysticrecords@hotmail.com>
2023-01-23 21:55:24 +0000

    created encode and decode functions
    created static alphabet and static cipher to compare user input secrept messgae

--- Section_10/Challenge/src/main.cpp
+++ Section_10/Challenge/src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string.h>
+
+const std::string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!.? "};
+const std::string encoder {"?MNB0V8CXZAS7DF1GHJKLQWERPO2IUYTqpwoi3ertyugh4fjdksl5azxm6n9cvb "};
+
+std::string encodetMsg(std::string msg) 
+{
+
+    for(char i: msg) 
+    {
+
+    }
+
+}
+    
+
+std::string decodeMsg(std::string msg) 
+{
+    
+}
 
 int main() 
 {
+    std::string secretMsg;
+    std::cout << "Please enter your secret message to encode" << std:: endl;
+    getline(cin, secretMsg);
+
+
     return 0;
 }
